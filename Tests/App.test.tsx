0ebadc5133d420ca25167d6da941e7f6ed09a8ae
adamhinton <adam.d.hinton@gmail.com>
2023-10-21 11:38:04 -0400

    delete vite-related files

--- Tests/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import App from "../src/App";
-import { render } from "@testing-library/react";
-import React from "react";
-
-test("[1] App starts without errors", () => {
-	render(<App></App>);
-});
