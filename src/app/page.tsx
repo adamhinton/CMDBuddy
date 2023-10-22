79750f43f99e4f85f63bd0f807f3b0e4ca485563
adamhinton <adam.d.hinton@gmail.com>
2023-10-21 21:24:35 -0400

    Config AMplify in page.tsx so now registration is working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 // prettier-ignore
 "use client";
-import Image from "next/image";
 import styles from "./page.module.css";
 import UserCreateForm from "@/ui-components/UserCreateForm";
+import { Amplify } from "aws-amplify";
+import config from "../aws-exports"; // adjust the path as needed
+
+Amplify.configure(config);
 
 export default function Home() {
 	return (
