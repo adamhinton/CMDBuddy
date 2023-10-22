c6d8d8003b8e5af4cbb2f749f9d07d1705b4fc13
adamhinton <adam.d.hinton@gmail.com>
2023-10-22 11:13:34 -0400

    Update pages.tsx with (temp deactivated) auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
 // prettier-ignore
 "use client";
 import styles from "./page.module.css";
-import UserCreateForm from "@/ui-components/UserCreateForm";
 import { Amplify } from "aws-amplify";
 import config from "../aws-exports"; // adjust the path as needed
+// import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+//https://ui.docs.amplify.aws/react/connected-components/authenticator/configuration
 
 Amplify.configure(config);
 
-export default function Home() {
+function Home() {
 	return (
 		<main className={styles.main}>
 			<h1>Adam Hinton</h1>
-			<UserCreateForm />
 		</main>
 	);
+}
+
+// export default withAuthenticator(Home);
+export default Home;
+
+{
+	/* <Authenticator>
+	{({ signOut, user }) => (
+		<div>
+			<h1>Adam Hinton</h1>
+			<button onClick={signOut}>Sign Out</button>
+		</div>
+	)}
+</Authenticator>; */
 }
