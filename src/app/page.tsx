9ce7092d61a15719e5547927f39378309aa2e24a
adamhinton <adam.d.hinton@gmail.com>
2023-10-22 11:25:14 -0400

    delete unneeded files and make sure app starts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 // prettier-ignore
 "use client";
-import styles from "./page.module.css";
 import { Amplify } from "aws-amplify";
 import config from "../aws-exports"; // adjust the path as needed
-// import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 //https://ui.docs.amplify.aws/react/connected-components/authenticator/configuration
@@ -12,7 +10,7 @@ Amplify.configure(config);
 
 function Home() {
 	return (
-		<main className={styles.main}>
+		<main>
 			<h1>Adam Hinton</h1>
 		</main>
 	);
