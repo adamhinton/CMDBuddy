9f285346d0a6ad01d3c78935bd96088329d7053c
adamhinton <adam.d.hinton@gmail.com>
2023-10-24 18:09:08 -0400

    Configure Amplify in app/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 // prettier-ignore
 // "use client";
 
+import { Amplify } from "aws-amplify";
+
 // Not sure I've done this right.
 //https://ui.docs.amplify.aws/react/connected-components/authenticator/configuration
+
+import config from "../aws-exports";
+Amplify.configure(config);
 
 function Home() {
 	return (
