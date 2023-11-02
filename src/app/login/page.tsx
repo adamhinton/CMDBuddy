3dfcf468b3d2dd925344bfcb27096f76aa7fd9fb
adamhinton <adam.d.hinton@gmail.com>
2023-11-01 21:45:41 -0400

    signout functionality now wipes user and command state

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,21 +5,40 @@ import { Amplify, Auth } from "aws-amplify";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
 import config from "../../../src/aws-exports";
+import Link from "next/link";
 Amplify.configure({ ...config, ssr: true });
+import { useDispatch } from "react-redux";
+import { logOutCommands } from "../../../redux/slices/commandsSlice";
+import { logOutUser } from "../../../redux/slices/authSlice";
 
 Authenticator;
 const Login = () => {
 	const currentUser = useSelector((state: RootState) => state.auth.user);
 
+	const dispatch = useDispatch();
+
+	const handleSignOut = async (
+		signOut: ((data?: Object | undefined) => void) | undefined
+	) => {
+		try {
+			signOut && signOut();
+			dispatch(logOutCommands());
+			dispatch(logOutUser());
+		} catch (error) {
+			console.error("Error signing out: ", error);
+		}
+	};
+
 	return (
 		<>
 			<h1>Log In</h1>
+			<Link href="/commands">Commands</Link>
 			<Authenticator>
 				{({ signOut, user }) => {
 					console.log("user:", user);
 					return (
 						<div>
-							<button onClick={signOut}>Sign Out</button>
+							<button onClick={() => handleSignOut(signOut)}>Sign Out</button>
 						</div>
 					);
 				}}
