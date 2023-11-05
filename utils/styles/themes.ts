93905bd3587213a44f51eb9ac89ad8d006b4ed5b
adamhinton <adam.d.hinton@gmail.com>
2023-11-05 12:57:13 -0500

    dummy utils/styles/themes.ts

---
+++ utils/styles/themes.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+
+// These are dummy themes for proof of concept, will flesh out later
+export const lightTheme: DefaultTheme = {
+	colors: {
+		background: "#fff",
+		text: "#333",
+	},
+};
+
+export const darkTheme: DefaultTheme = {
+	colors: {
+		background: "#1a1a1a",
+		text: "#f5f5f5",
+	},
+};
