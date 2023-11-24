17608dda41e46afaeaf27e106bb777018be22ee6
adamhinton <adam.d.hinton@gmail.com>
2023-11-24 13:53:07 -0500

    add event done

--- amplify/backend/function/signUpCognitoFirst/src/index.js
+++ amplify/backend/function/signUpCognitoFirst/src/index.js
@@ -1,17 +1,61 @@
-
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+
+const fetch = require("node-fetch");
+
+exports.handler = async (event, context) => {
+	const GRAPHQL_ENDPOINT = process.env.API_CMDBUDDY_GRAPHQLAPIENDPOINTOUTPUT;
+	const GRAPHQL_API_KEY = process.env.API_CMDBUDDY_GRAPHQLAPIKEYOUTPUT;
+
+	const query = /* GraphQL */ `
+		mutation CREATE_USER($input: CreateUserInput!) {
+			createUser(input: $input) {
+				email
+			}
+		}
+	`;
+
+	const variables = {
+		input: {
+			email: event.request.userAttributes.email,
+		},
+	};
+
+	const options = {
+		method: "POST",
+		headers: {
+			"x-api-key": GRAPHQL_API_KEY,
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ query, variables }),
+	};
+
+	const response = {};
+
+	try {
+		const res = await fetch(GRAPHQL_ENDPOINT, options);
+		response.data = await res.json();
+		if (response.data.errors) response.statusCode = 400;
+	} catch (error) {
+		response.statusCode = 400;
+		response.body = {
+			errors: [
+				{
+					message: error.message,
+					stack: error.stack,
+				},
+			],
+		};
+	}
+
+	return {
+		...response,
+		body: JSON.stringify(response.body),
+	};
 };
