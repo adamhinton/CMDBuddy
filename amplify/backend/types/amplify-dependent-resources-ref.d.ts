66d4bea229625598d94d628816246e2741297487
adamhinton <adam.d.hinton@gmail.com>
2023-11-24 16:24:47 -0500

    Push changes to amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -18,7 +18,7 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
-    "signUpCognitoFirst": {
+    "signUpSyncCognitoFirst": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
