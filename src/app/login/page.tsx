b6f85c5f2d3aca09e735ce6fa644a22a0c43ba3c
adamhinton <adam.d.hinton@gmail.com>
2023-11-26 12:50:22 -0500

    Users can now change password and delete account in Cognito. Now to sync the deletion with dynamodb

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,24 @@
 "use client";
-// Not sure I've done this right.
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, AccountSettings } from "@aws-amplify/ui-react";
 import { Amplify, Auth } from "aws-amplify";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
 import config from "../../../src/aws-exports";
 import Link from "next/link";
-Amplify.configure({ ...config, ssr: true });
 import { useAuthActions } from "../../../utils/authUtils";
-import { useEffect } from "react";
+// import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+Amplify.configure({ ...config, ssr: true });
 
 const Login = () => {
 	const currentUser = useSelector((state: RootState) => state.auth.user);
 	const { setUserAndCommandsToState, logOut } = useAuthActions();
+	// These two states are UIs that show when a user clicks the change password button or delete account button
+	const [showChangePassword, setShowChangePassword] = useState(false);
+	const [showDeleteAccount, setShowDeleteAccount] = useState(false);
+	const router = useRouter();
 
 	useEffect(() => {
 		const checkUser = async () => {
@@ -23,17 +29,16 @@ const Login = () => {
 				logOut();
 			}
 		};
-
 		checkUser();
 	}, [currentUser, logOut]);
 
 	const handleLogin = async (user: any) => {
 		if (user) {
 			await setUserAndCommandsToState(user);
+			router.push("/commands");
 		}
 	};
 
-	//TODO: Fix this any type
 	const handleSignOut = async (signOut?: any) => {
 		try {
 			if (signOut) {
@@ -43,6 +48,20 @@ const Login = () => {
 		} catch (error) {
 			console.error("Error signing out: ", error);
 		}
+	};
+
+	const handleSuccess = () => {
+		router.push("/commands");
+	};
+
+	const toggleChangePassword = () => {
+		setShowChangePassword(true);
+		setShowDeleteAccount(false);
+	};
+
+	const toggleDeleteAccount = () => {
+		setShowDeleteAccount(true);
+		setShowChangePassword(false);
 	};
 
 	return (
@@ -57,7 +76,20 @@ const Login = () => {
 					return (
 						<div>
 							{currentUser ? (
-								<button onClick={() => handleSignOut(signOut)}>Sign Out</button>
+								<>
+									<button onClick={() => handleSignOut(signOut)}>
+										Sign Out
+									</button>
+									<button onClick={toggleChangePassword}>
+										Change Password
+									</button>
+									{/* Clicking this doesn't immediately delete account, just pulls up the UI to do so */}
+									<button onClick={toggleDeleteAccount}>Delete Account</button>
+									{showChangePassword && (
+										<AccountSettings.ChangePassword onSuccess={handleSuccess} />
+									)}
+									{showDeleteAccount && <AccountSettings.DeleteUser />}
+								</>
 							) : (
 								"Please sign in"
 							)}
