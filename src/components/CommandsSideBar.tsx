cfbb4b40d8f52074192d3328fcb762b79b5e11d2
adamhinton <adam.d.hinton@gmail.com>
2023-12-06 00:51:34 -0500

    Prototype CommandsSideBar.tsx

--- src/components/CommandsSideBar.tsx
+++ src/components/CommandsSideBar.tsx
@@ -1,5 +1,74 @@
-const CommandsSideBar = () => {
-	return <ol>Sidebar</ol>;
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+
+const SideBarContainer = styled.div`
+	width: 250px;
+	/* background-color: ${({ theme }) => theme.sidebarBg}; */
+	color: ${({ theme }) => theme.text};
+	height: 100vh;
+	overflow-y: auto;
+	padding: 10px;
+`;
+
+const CommandItem = styled.div`
+	padding: 10px;
+	margin-bottom: 5px;
+	border-radius: 5px;
+	background-color: ${({ theme }) => theme.commandBg};
+	color: ${({ theme }) => theme.commandText};
+	cursor: pointer;
+	&:hover {
+		background-color: ${({ theme }) => theme.commandHoverBg};
+	}
+`;
+
+const SideBar = () => {
+	const commands = useSelector((state: RootState) => state.commands.commands); // Mock selector
+
+	return (
+		<SideBarContainer>
+			{commands?.map((command, index) => (
+				<Command key={index} title={command.title} />
+			))}
+		</SideBarContainer>
+	);
 };
 
-export default CommandsSideBar;
+export default SideBar;
+
+const CommandContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const Title = styled.span`
+	flex-grow: 1;
+`;
+
+const EditButton = styled.button`
+	margin-right: 10px;
+	background: none;
+	border: none;
+	cursor: pointer;
+`;
+
+const DeleteButton = styled.button`
+	background: none;
+	border: none;
+	cursor: pointer;
+`;
+
+const Command = ({ title }: { title: string }) => {
+	return (
+		<CommandContainer>
+			<Title>{title}</Title>
+			<EditButton>✏️</EditButton>
+			<DeleteButton>🗑️</DeleteButton>
+		</CommandContainer>
+	);
+};
