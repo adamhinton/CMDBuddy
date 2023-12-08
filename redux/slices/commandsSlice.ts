eecaff8af20b8793c97a06199b3d61d998cd7b50
adamhinton <adam.d.hinton@gmail.com>
2023-12-07 20:28:26 -0500

    Helpful comments and update imports

--- redux/slices/commandsSlice.ts
+++ redux/slices/commandsSlice.ts
@@ -1,3 +1,7 @@
+// README:
+// This is Redux state management for Commands AND PARAMETERS, they all live happily in the same state object. I did it this way because each Parameter has only one Command so it didn't make sense to separate them.
+// Note, this is only client state management. Any database stuff is done elsewhere, often triggered by whatever called these reducers.
+
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CMDBuddyCommand } from "../../utils/zod/CommandSchema";
 
@@ -16,15 +20,34 @@ export const commandsSlice = createSlice({
 		setCommands: (state, action: PayloadAction<CMDBuddyCommand[]>) => {
 			state.commands = action.payload;
 		},
+
 		logOutCommands: (state) => {
 			state.commands = null;
 		},
+
 		addCommand: (state, action: PayloadAction<CMDBuddyCommand>) => {
 			if (state.commands) {
 				state.commands = [action.payload, ...state.commands];
 			} else {
 				state.commands = [action.payload];
 			}
+		},
+
+		editCommandTitle: (
+			state,
+			action: PayloadAction<{ commandId: string; newTitle: string }>
+		) => {
+			const { commandId, newTitle } = action.payload;
+			const command = state.commands?.find((cmd) => cmd.id === commandId);
+			if (command) {
+				command.title = newTitle;
+			}
+		},
+
+		deleteCommand: (state, action: PayloadAction<string>) => {
+			const commandId = action.payload;
+			state.commands =
+				state.commands?.filter((cmd) => cmd.id !== commandId) || null;
 		},
 	},
 });
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,99 +1,136 @@
 "use client";
 
-// TODO:
-// Edit field shows up, now instate submit logic
-// Delete logic
-// DnD
-// Horizontal issue
-
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../redux/store";
+import { editCommandTitle, deleteCommand } from "../../redux/slices/commandsSlice";
+import { API, graphqlOperation } from 'aws-amplify';
+import { updateCommand, deleteCommand as deleteCommandMutation } from '@/graphql/mutations';
+
 
 const SideBarContainer = styled.div`
-	width: 250px;
-	/* background-color: ${({ theme }) => theme.sidebarBg}; */
-	color: ${({ theme }) => theme.text};
-	height: 100vh;
-	overflow-y: auto;
-	padding: 10px;
-`;
-
-const CommandItem = styled.div`
-	padding: 10px;
-	margin-bottom: 5px;
-	border-radius: 5px;
-	background-color: ${({ theme }) => theme.commandBg};
-	color: ${({ theme }) => theme.commandText};
-	cursor: pointer;
-	&:hover {
-		background-color: ${({ theme }) => theme.commandHoverBg};
-	}
+  width: 250px;
+  color: ${({ theme }) => theme.text};
+  height: 100vh;
+  overflow-y: auto;
+  padding: 10px;
 `;
 
 const CommandContainer = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Title = styled.span`
-	flex-grow: 1;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	white-space: nowrap;
+  flex-grow: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 const EditInput = styled.input`
-	flex-grow: 1;
-	border: none;
-	padding: 4px;
+  flex-grow: 1;
+  border: none;
+  padding: 4px;
 `;
 
 const EditButton = styled.button`
-	margin-right: 10px;
-	background: none;
-	border: none;
-	cursor: pointer;
+  margin-right: 10px;
+  background: none;
+  border: none;
+  cursor: pointer;
 `;
 
 const DeleteButton = styled.button`
-	background: none;
-	border: none;
-	cursor: pointer;
+  background: none;
+  border: none;
+  cursor: pointer;
 `;
 
-const Command = ({ title }: { title: string }) => {
-	const [isEditing, setIsEditing] = useState(false);
-	const [editedTitle, setEditedTitle] = useState(title);
+const ConfirmIcon = styled.span`
+  margin-left: 5px;
+`;
 
-	return (
-		<CommandContainer>
-			{isEditing ? (
-				<EditInput
-					value={editedTitle}
-					onChange={(e) => setEditedTitle(e.target.value)}
-				/>
-			) : (
-				<Title>{title}</Title>
-			)}
-			<EditButton onClick={() => setIsEditing(!isEditing)}>✏️</EditButton>
-			<DeleteButton>🗑️</DeleteButton>
-		</CommandContainer>
-	);
+const Command = ({ title, commandID }: { title: string; commandID: string }) => {
+  const dispatch = useDispatch();
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
+  const [showConfirm, setShowConfirm] = useState(false);
+  const editInputRef = useRef<HTMLInputElement>(null);
+
+  const handleCommandTitlesEditSubmit = async () => {
+    // Optimistic UI update
+    dispatch(editCommandTitle({ commandId: commandID, newTitle: editedTitle }));
+
+    // Update the database
+    const commandDetails = { id: commandID, title: editedTitle };
+    await API.graphql(graphqlOperation(updateCommand, { input: commandDetails }));
+
+    setIsEditing(false);
+  };
+
+  const handleCommandDelete = async () => {
+    // Delete the command from the database
+    await API.graphql(graphqlOperation(deleteCommandMutation, { input: { id: commandID } }));
+
+    // Optimistic UI update
+    dispatch(deleteCommand(commandID));
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        editInputRef.current &&
+        !editInputRef.current.contains(e.target as Node)
+      ) {
+        setIsEditing(false);
+        setEditedTitle(title);
+      }
+    };
+
+    if (isEditing) {
+      document.addEventListener("click", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isEditing, title]);
+
+  return (
+    <CommandContainer>
+      {isEditing ? (
+        <EditInput
+          ref={editInputRef}
+          value={editedTitle}
+          onChange={(e) => setEditedTitle(e.target.value)}
+          onBlur={handleCommandTitlesEditSubmit}
+          onKeyDown={(e) => e.key === "Enter" && handleCommandTitlesEditSubmit()}
+        />
+      ) : (
+        <Title>{title}</Title>
+      )}
+      <EditButton onClick={() => setIsEditing(!isEditing)}>✏️</EditButton>
+      <DeleteButton onClick={() => showConfirm ? handleCommandDelete() : setShowConfirm(true)}>
+        🗑️
+      </DeleteButton>
+      {showConfirm && <ConfirmIcon>✅</ConfirmIcon>}
+    </CommandContainer>
+  );
 };
 
 const SideBar = () => {
-	const commands = useSelector((state: RootState) => state.commands.commands);
+  const commands = useSelector((state: RootState) => state.commands.commands);
 
-	return (
-		<SideBarContainer>
-			{commands?.map((command, index) => (
-				<Command key={index} title={command.title} />
-			))}
-		</SideBarContainer>
-	);
+  return (
+    <SideBarContainer>
+      {commands?.map((command, index) => (
+        <Command key={index} title={command.title} commandID={command.id} />
+      ))}
+    </SideBarContainer>
+  );
 };
 
 export default SideBar;
