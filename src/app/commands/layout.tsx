e7b88020591a25631e4a8201fe01e795d761e8fb
adamhinton <adam.d.hinton@gmail.com>
2023-12-11 22:03:17 -0500

    fix import issue

--- src/app/commands/layout.tsx
+++ src/app/commands/layout.tsx
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-// Sidebar is the list of user's Commands
-// They can edit the Command's title, rearrange their order or delete commands
-// Clicking a Command brings it up in the main UI
-=======
-import CommandsSideBar from "@/components/CommandsSideBar";
->>>>>>> 87215d815e18d671c181df533d899ee70dadbc81
+import CommandInSideBar from "@/components/SideBar/CommandInSideBar";
 import SideBar from "@/components/SideBar/SideBar";
 
 export default function CommandsLayout({
