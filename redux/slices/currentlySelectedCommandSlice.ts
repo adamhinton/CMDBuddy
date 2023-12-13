0d77f6e255914d80ef1c05dea7fe86546b52e7cb
adamhinton <adam.d.hinton@gmail.com>
2023-12-13 18:33:46 -0500

    skeleton currentlySelectedCommandSlice.ts

---
+++ redux/slices/currentlySelectedCommandSlice.ts
@@ -0,0 +1,3 @@
+// README:
+// This tracks what command (if any) the user is currently filling out Parameter values for.
+// The user selects a Command from Sidebar and that pops up in CommandExecutionForm.tsx
--- src/components/CommandExecutionForm.tsx
+++ src/components/CommandExecutionForm.tsx
@@ -1,3 +1,5 @@
 // README:
 // In this form, the user enters in values for Parameters.
 // They should have already created a Command and its Parameters. If they haven't, see CommandCreationForm.tsx
+// This page dynamically generates the Command and its Parameters with the inputted values
+// And displays the completed command on the page.
