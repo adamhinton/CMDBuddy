cb6edea482ba219fbcf61b1d559f9988756c1035
adamhinton <adam.d.hinton@gmail.com>
2023-12-13 18:44:21 -0500

    better slice variable naming: activeCommandSlice

---
+++ redux/slices/activeCommandSlice.ts
@@ -0,0 +1,34 @@
+// README:
+// This tracks what command (if any) the user is currently filling out Parameter values for.
+// The user selects a Command from Sidebar and that pops up in CommandExecutionForm.tsx
+
+// NOTE: This is for Commands that already exist in the DB; they are filling out values for the Parameter keys.
+// If they want to make a new Command with new Parameter keys, they should be filling out CommandCreationForm.tsx
+
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CMDBuddyCommand } from "../../utils/zod/CommandSchema";
+
+interface activeCommandState {
+	activeCommand: null | CMDBuddyCommand;
+}
+
+const initialState: activeCommandState = {
+	activeCommand: null,
+};
+
+export const activeCommandSlice = createSlice({
+	name: "activeCommand",
+	initialState,
+	reducers: {
+		setactiveCommand: (state, action: PayloadAction<CMDBuddyCommand>) => {
+			state.activeCommand = action.payload;
+		},
+
+		deleteactiveCommand: (state) => {
+			state.activeCommand = null;
+		},
+	},
+});
+
+export const { setactiveCommand } = activeCommandSlice.actions;
+export default activeCommandSlice.reducer;
--- redux/store.ts
+++ redux/store.ts
@@ -3,7 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import commandsReducer from "./slices/commandsSlice";
 import darkModeReducer from "./slices/darkModeSlice";
-import currentlySelectedCommandReducer from "./slices/currentlySelectedCommandSlice";
+import activeCommandReducer from "./slices/activeCommandSlice";
 
 if (typeof authReducer !== "function") {
 	console.error("authReducer is not a valid reducer function");
@@ -14,7 +14,7 @@ export const store = configureStore({
 		auth: authReducer,
 		commands: commandsReducer,
 		darkMode: darkModeReducer,
-		currentlySelectedCommand: currentlySelectedCommandReducer,
+		activeCommand: activeCommandReducer,
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({
