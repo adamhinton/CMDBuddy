40102cf79ad94dea752bcbf6218a10331d900fed
adamhinton <adam.d.hinton@gmail.com>
2023-12-13 20:06:48 -0500

    Fixed issue where commands/activecommandID somehow deleted the cognito logged in user.
    Don't ask me how or why this worked.

--- src/app/commands/[activeCommandID]/page.tsx
+++ src/app/commands/[activeCommandID]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Amplify, Auth } from "aws-amplify";
 import config from "../../../aws-exports";
-Amplify.configure({ config });
+Amplify.configure({ ...config });
 
 import { useEffect } from "react";
 
