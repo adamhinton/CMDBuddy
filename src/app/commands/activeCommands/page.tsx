30466ad5cf256c0bffccce854b5ce9687e22cc21
adamhinton <adam.d.hinton@gmail.com>
2023-12-15 11:29:49 -0500

    activecommands/page.tsx now gets the active Commands and loops over them

--- src/app/commands/activeCommands/page.tsx
+++ src/app/commands/activeCommands/page.tsx
@@ -2,8 +2,37 @@
 
 import { Amplify } from "aws-amplify";
 import config from "../../../aws-exports";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../redux/store";
+import { CMDBuddyCommand } from "../../../../utils/zod/CommandSchema";
 Amplify.configure(config);
 
 export default function ActiveCommandsPage() {
-	return <div>Active Commands here</div>;
+	// This is all the ids of the commands the user has selected
+	const activeCommandIDs = useSelector((state: RootState) => {
+		return state.activeCommands.activeCommands;
+	});
+
+	// This is all the user's commands
+	const allCommands: CMDBuddyCommand[] | null = useSelector(
+		(state: RootState) => {
+			return state.commands.commands;
+		}
+	);
+
+	// Filter 'allCommands' to include only those present in 'activeCommandIDs'
+	const activeCommands: CMDBuddyCommand[] =
+		allCommands?.filter((command) => activeCommandIDs.includes(command.id)) ||
+		[];
+
+	console.log("activeCommands:", activeCommands);
+
+	return (
+		<div>
+			{activeCommands.map((command) => (
+				// Render each active command here
+				<div key={command.id}>Command Title: {command.title}</div>
+			))}
+		</div>
+	);
 }
