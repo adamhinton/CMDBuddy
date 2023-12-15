0906462aba9719174bbc66777c88d7512ada12c0
adamhinton <adam.d.hinton@gmail.com>
2023-12-15 17:58:15 -0500

    wip: Adding parameter inputs to PEF

--- src/components/CommandExecutionComponents/CommandExecutionForm.tsx
+++ src/components/CommandExecutionComponents/CommandExecutionForm.tsx
@@ -8,6 +8,8 @@ import { CMDBuddyCommand } from "../../../utils/zod/CommandSchema";
 import { removeSingleActiveCommand } from "../../../redux/slices/activeCommandsSlice";
 import { useDispatch } from "react-redux";
 import ParameterExecutionForm from "./ParameterExecutionForm";
+import React from "react";
+import { useForm, FormProvider } from "react-hook-form";
 
 const removeCommandOnClick = (
 	e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
@@ -19,24 +21,39 @@ const removeCommandOnClick = (
 };
 
 const CommandExecutionForm = ({ command }: { command: CMDBuddyCommand }) => {
+	const methods = useForm({
+		defaultValues: {
+			// We'll integrate parameters here later
+		},
+	});
+
 	const dispatch = useDispatch();
 	const parameters = command.parameters;
 
+	const { watch } = methods;
+	const parameterValues = watch(); // Tracks all parameter values
+	console.log("parameterValues:", parameterValues);
+
 	return (
-		<section>
-			<h3>{command.title}</h3>
-			{parameters?.map((param) => {
-				return <ParameterExecutionForm parameter={param} key={param.id} />;
-			})}
-			<button
-				onClick={(e) => {
-					removeCommandOnClick(e, command.id, dispatch);
-				}}
-			>
-				Exit this command
-			</button>
-		</section>
+		<FormProvider {...methods}>
+			<form>
+				<section>
+					<header>
+						<h3>{command.title}</h3>
+					</header>
+					{parameters?.map((param) => {
+						return <ParameterExecutionForm parameter={param} key={param.id} />;
+					})}
+					<button
+						onClick={(e) => {
+							removeCommandOnClick(e, command.id, dispatch);
+						}}
+					>
+						Exit this command
+					</button>
+				</section>
+			</form>
+		</FormProvider>
 	);
 };
-
 export default CommandExecutionForm;
--- src/components/CommandExecutionComponents/ParameterExecutionForm.tsx
+++ src/components/CommandExecutionComponents/ParameterExecutionForm.tsx
@@ -1,11 +1,128 @@
+import React from "react";
+import { FieldValues, RegisterOptions, useFormContext } from "react-hook-form";
 import { CMDBuddyParameter } from "../../../utils/zod/ParameterSchema";
+
+const bob: RegisterOptions<FieldValues, string>;
 
 const ParameterExecutionForm = ({
 	parameter,
 }: {
 	parameter: CMDBuddyParameter;
 }) => {
-	return <div>{parameter.name}</div>;
+	const { register } = useFormContext();
+
+	// Render input based on parameter type
+	const renderInputField = () => {
+		switch (parameter.type) {
+			case "STRING":
+				return (
+					<input
+						type="text"
+						{...register(parameter.name, {
+							required: !parameter.isNullable,
+							maxLength: parameter.maxLength,
+							minLength: parameter.minLength,
+							pattern: parameter.validationRegex
+								? new RegExp(parameter.validationRegex)
+								: undefined,
+							value: parameter.defaultValue,
+						})}
+					/>
+				);
+			case "INT":
+				return (
+					<input
+						type="number"
+						{...register(parameter.name, {
+							required: !parameter.isNullable,
+							max: parameter.maxValue,
+							min: parameter.minValue,
+							value: parameter.defaultValue,
+						})}
+					/>
+				);
+			case "BOOLEAN":
+				return (
+					// Assuming "true" and "false" are the values to be used
+					<div>
+						<label>
+							<input
+								type="radio"
+								value="true"
+								{...register(parameter.name, {
+									required: !parameter.isNullable,
+									value: parameter.defaultValue === "true",
+								})}
+							/>
+							True
+						</label>
+						<label>
+							<input
+								type="radio"
+								value="false"
+								{...register(parameter.name, {
+									required: !parameter.isNullable,
+									defaultValue: parameter.defaultValue === "false",
+								})}
+							/>
+							False
+						</label>
+					</div>
+				);
+			case "DROPDOWN":
+				return (
+					<select
+						{...register(parameter.name, {
+							required: !parameter.isNullable,
+							value: parameter.defaultValue,
+						})}
+					>
+						{parameter.allowedValues?.map((value) => (
+							<option key={value} value={value}>
+								{value}
+							</option>
+						))}
+					</select>
+				);
+			case "FLAG":
+				return (
+					// Assuming "On" and "Off" are the values to be used
+					<div>
+						<label>
+							<input
+								type="radio"
+								value="On"
+								{...register(parameter.name, {
+									required: !parameter.isNullable,
+									defaultValue: parameter.defaultValue === "On",
+								})}
+							/>
+							On
+						</label>
+						<label>
+							<input
+								type="radio"
+								value="Off"
+								{...register(parameter.name, {
+									required: !parameter.isNullable,
+									defaultValue: parameter.defaultValue === "Off",
+								})}
+							/>
+							Off
+						</label>
+					</div>
+				);
+			default:
+				return <p>Unsupported parameter type</p>;
+		}
+	};
+
+	return (
+		<div>
+			<label htmlFor={parameter.name}>{parameter.name}</label>
+			{renderInputField()}
+		</div>
+	);
 };
 
 export default ParameterExecutionForm;
