6991471a6939d18d2bffec044425a2d8ae8c1ca3
adamhinton <adam.d.hinton@gmail.com>
2023-12-15 11:20:29 -0500

    skeleton CEF.tsx

--- src/components/CommandExecutionForm.tsx
+++ src/components/CommandExecutionForm.tsx
@@ -3,3 +3,11 @@
 // They should have already created a Command and its Parameters. If they haven't, see CommandCreationForm.tsx
 // This page dynamically generates the Command and its Parameters with the inputted values
 // And displays the completed command on the page.
+
+import { CMDBuddyCommand } from "../../utils/zod/CommandSchema";
+
+const CommandExecutionForm = (command: CMDBuddyCommand) => {
+	return <div>Command: {command.title}</div>;
+};
+
+export default CommandExecutionForm;
