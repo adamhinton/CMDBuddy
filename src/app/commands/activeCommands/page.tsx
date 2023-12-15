0af93aa91eee87612a9e174cd0d71ae58d7aebfc
adamhinton <adam.d.hinton@gmail.com>
2023-12-15 11:20:10 -0500

    rejiffy activecommands/page

--- src/app/commands/activeCommands/page.tsx
+++ src/app/commands/activeCommands/page.tsx
@@ -1,21 +1,9 @@
 "use client";
+
 import { Amplify } from "aws-amplify";
 import config from "../../../aws-exports";
-Amplify.configure({ ...config });
-import { RootState } from "../../../../redux/store";
+Amplify.configure(config);
 
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-
-export default function Page() {
-	const activeCommandIDs = useSelector(
-		(state: RootState) => state.activeCommands.activeCommands
-	);
-	console.log("activeCommandIDs:", activeCommandIDs);
-
-	useEffect(() => {
-		console.log("commands/active/page useEffect");
-	});
-
-	return <div>Multiple Commands Here</div>;
+export default function ActiveCommandsPage() {
+	return <div>Active Commands here</div>;
 }
