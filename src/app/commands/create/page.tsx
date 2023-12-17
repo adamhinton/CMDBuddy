5953aee01fe37bbb5835d304ac519113535640fc
adamhinton <adam.d.hinton@gmail.com>
2023-12-17 13:19:44 -0500

    Configure amplify in generate

--- src/app/commands/create/page.tsx
+++ src/app/commands/create/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Amplify } from "aws-amplify";
+import config from "../../../aws-exports";
+Amplify.configure({ config, ssr: true });
+
 import CommandCreationForm from "@/components/CommandCreationComponents/CommandCreationForm";
 
 const CreateCommandsPage = () => {
