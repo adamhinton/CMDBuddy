0ccf7ac91b99b719461dec227649703f80d2d1ee
adamhinton <adam.d.hinton@gmail.com>
2023-12-16 21:07:07 -0500

    WIP: Flesh out Tabs.tsx but it has errors and bugs

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,7 +1,83 @@
 "use client";
 
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import styled, { useTheme } from "styled-components";
+
+const TabContainer = styled.div`
+	display: flex;
+	background: ${({ theme }) => theme.colors.tabBackground};
+	padding: 10px 0;
+`;
+
+const Tab = styled.button`
+	background: ${({ theme, active }) =>
+		active ? theme.colors.activeTabBackground : "transparent"};
+	color: ${({ theme }) => theme.colors.text};
+	border: none;
+	padding: 10px 15px;
+	cursor: pointer;
+	&:hover {
+		background: ${({ theme }) => theme.colors.hoverTabBackground};
+	}
+`;
+
+const SubTabContainer = styled.div`
+	display: flex;
+	background: ${({ theme }) => theme.colors.activeTabBackground};
+	padding: 5px 0;
+	margin-top: 5px;
+`;
+
+const SubTab = styled(Tab)`
+	padding: 5px 10px;
+`;
+
 const Tabs = () => {
-	return <nav>Tabs here</nav>;
+	const [activeTab, setActiveTab] = useState("/");
+	const router = useRouter();
+	const theme = useTheme();
+
+	const handleTabClick = (path: string) => {
+		router.push(path);
+		setActiveTab(path);
+	};
+
+	return (
+		<nav>
+			<TabContainer>
+				<Tab
+					onClick={() => handleTabClick("/commands")}
+					active={activeTab === "/commands"}
+				>
+					Commands
+				</Tab>
+				<Tab
+					onClick={() => handleTabClick("/about")}
+					active={activeTab === "/about"}
+				>
+					About
+				</Tab>
+				<Tab
+					onClick={() => handleTabClick("/login")}
+					active={activeTab === "/login"}
+				>
+					Login
+				</Tab>
+			</TabContainer>
+			{activeTab === "/commands" && (
+				<SubTabContainer>
+					<SubTab onClick={() => handleTabClick("/commands/generate")}>
+						Generate
+					</SubTab>
+					<SubTab onClick={() => handleTabClick("/commands/edit")}>Edit</SubTab>
+					<SubTab onClick={() => handleTabClick("/commands/create")}>
+						Create
+					</SubTab>
+				</SubTabContainer>
+			)}
+		</nav>
+	);
 };
 
 export default Tabs;
--- utils/styles/themes.ts
+++ utils/styles/themes.ts
@@ -5,6 +5,9 @@ export const lightTheme: DefaultTheme = {
 	colors: {
 		background: "#fff",
 		text: "#333",
+		tabBackground: "#e0e0e0",
+		activeTabBackground: "#c0c0c0",
+		hoverTabBackground: "#d0d0d0",
 	},
 };
 
@@ -12,5 +15,8 @@ export const darkTheme: DefaultTheme = {
 	colors: {
 		background: "#1a1a1a",
 		text: "#f5f5f5",
+		tabBackground: "#333",
+		activeTabBackground: "#222",
+		hoverTabBackground: "#444",
 	},
 };
