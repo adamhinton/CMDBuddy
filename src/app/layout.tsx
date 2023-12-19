7f9057df4e907518d67f7bf375fec712c2f63c90
adamhinton <adam.d.hinton@gmail.com>
2023-12-19 14:08:47 -0500

    Rejigger theming to toggle between light and dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
 		<html lang="en">
 			<body>
 				<Providers>
-					<ThemeProviderWrapper theme={lightTheme}>
+					<ThemeProviderWrapper>
 						<AuthClientComponent />
 						<Header />
 						<main>{children}</main>
--- src/components/ThemeProviderWrapper.tsx
+++ src/components/ThemeProviderWrapper.tsx
@@ -1,14 +1,29 @@
 "use client";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { DefaultTheme, ThemeProvider } from "styled-components";
+import { RootState } from "../../redux/store";
+import { darkTheme, lightTheme } from "../../utils/styles/themes";
 
 // The props should be passed as a single object and destructured within the function parameter list
 const ThemeProviderWrapper = ({
-	theme,
+	// theme,
 	children,
 }: {
-	theme: DefaultTheme;
+	// theme: DefaultTheme;
 	children: React.ReactNode;
 }) => {
+	const isDarkMode = useSelector(
+		(state: RootState) => state.darkMode.isDarkMode
+	);
+	console.log("isDarkMode TPR:", isDarkMode);
+	const [theme, setTheme] = useState(darkTheme);
+
+	useEffect(() => {
+		setTheme(isDarkMode ? darkTheme : lightTheme);
+	}, [isDarkMode]);
+
+	console.log("theme:", theme);
 	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
 
