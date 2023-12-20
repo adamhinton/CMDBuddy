796b487551ac087f1da887b5674aba261c331bf2
adamhinton <adam.d.hinton@gmail.com>
2023-12-19 23:23:31 -0500

    Header styling and theming

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,7 @@ import {
 	setDarkModeLocalStorage,
 	toggleUserDarkModeInDB,
 } from "../../utils/darkModeUtils";
+import styled from "styled-components";
 
 const Header = () => {
 	const userID = useSelector((state: RootState) => state.auth.user?.id);
@@ -44,11 +45,78 @@ const Header = () => {
 	};
 
 	return (
-		<header>
-			<h2>Header</h2>
-			<button onClick={(e) => darkModeOnClick(e)}>Toggle Dark mode</button>
-		</header>
+		<StyledHeader>
+			<TitleContainer>
+				<Title>CMDBuddy</Title>
+				<SubTitle>by Adam Hinton</SubTitle>
+			</TitleContainer>
+			<Nav>
+				<StyledLink
+					href="https://github.com/adamhinton/CMDBuddy"
+					target="_blank"
+				>
+					Source
+				</StyledLink>
+				<StyledLink
+					href="https://www.linkedin.com/in/adam-hinton/"
+					target="_blank"
+				>
+					LinkedIn
+				</StyledLink>
+				<StyledLink
+					href="https://master.dorqegfj1nrtm.amplifyapp.com/"
+					target="_blank"
+				>
+					Risk BattleOdds Calculator
+				</StyledLink>
+			</Nav>
+			<DarkModeToggle onClick={(e) => darkModeOnClick(e)}>
+				{isDarkMode ? "🌞" : "🌜"}
+			</DarkModeToggle>
+		</StyledHeader>
 	);
 };
 
 export default Header;
+
+const StyledHeader = styled.header`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1rem;
+	background-color: ${({ theme }) => theme.header.background};
+	color: ${({ theme }) => theme.header.text};
+`;
+
+const TitleContainer = styled.div``;
+
+const Title = styled.h1`
+	margin: 0;
+	font-size: 1.5rem;
+`;
+
+const SubTitle = styled.h2`
+	margin: 0;
+	font-size: 1rem;
+`;
+
+const Nav = styled.nav`
+	display: flex;
+	gap: 1rem;
+`;
+
+const StyledLink = styled.a`
+	color: ${({ theme }) => theme.header.link};
+	text-decoration: none;
+	&:hover {
+		text-decoration: underline;
+	}
+`;
+
+const DarkModeToggle = styled.button`
+	background: none;
+	border: none;
+	color: ${({ theme }) => theme.header.toggleIcon};
+	cursor: pointer;
+	font-size: 1.5rem;
+`;
--- utils/styles/themes.ts
+++ utils/styles/themes.ts
@@ -9,6 +9,12 @@ export const lightTheme: DefaultTheme = {
 		activeTabBackground: "#c0c0c0",
 		hoverTabBackground: "#d0d0d0",
 	},
+	header: {
+		background: "#E0F7FA", // Light blue background
+		text: "#0D47A1", // Dark blue text
+		link: "#004D40", // Teal for links
+		toggleIcon: "#FFCA28", // Sun icon color
+	},
 };
 
 export const darkTheme: DefaultTheme = {
@@ -18,5 +24,11 @@ export const darkTheme: DefaultTheme = {
 		tabBackground: "#333",
 		activeTabBackground: "#222",
 		hoverTabBackground: "#444",
+	},
+	header: {
+		background: "#263238", // Dark slate background
+		text: "#ECEFF1", // Light gray text
+		link: "#80CBC4", // Soft teal for links
+		toggleIcon: "#FFEB3B", // Moon icon color
 	},
 };
