66ece73b5fb8e6b53306d0be5b068585d003f102
adamhinton <adam.d.hinton@gmail.com>
2023-12-30 13:07:24 -0500

    basic commands/edit/page

--- src/app/commands/edit/page.tsx
+++ src/app/commands/edit/page.tsx
@@ -2,10 +2,32 @@
 
 import { Amplify } from "aws-amplify";
 import config from "../../../aws-exports";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../redux/store";
+import CommandCreationOrEditForm, {
+	CommandCreationFormSchema,
+} from "@/components/CommandCreationComponents/CommandCreationForm";
 Amplify.configure({ ...config, ssr: true });
 
 const CommandsEditPage = () => {
-	return <h3>Edit Placeholder</h3>;
+	const commandToEdit = useSelector((state: RootState) => {
+		return state.commandToEdit.commandToEdit;
+	});
+
+	console.log("commandToEdit in edit/page:", commandToEdit);
+
+	return (
+		<section>
+			{!commandToEdit && <h3>Click edit icon in sidebar to edit a command</h3>}
+			{commandToEdit && <h3>Edit your command here</h3>}
+			{commandToEdit && (
+				<CommandCreationOrEditForm
+					componentMode="editExistingCommand"
+					commandToEdit={commandToEdit}
+				></CommandCreationOrEditForm>
+			)}
+		</section>
+	);
 };
 
 export default CommandsEditPage;
