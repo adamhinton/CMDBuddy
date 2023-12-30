798e79114158bcf41b12cdcc85b786fc422a8708
adamhinton <adam.d.hinton@gmail.com>
2023-12-30 12:12:14 -0500

    helpful comments

--- src/components/SideBar/CommandInSideBar.tsx
+++ src/components/SideBar/CommandInSideBar.tsx
@@ -1,7 +1,8 @@
 // README:
 // This is an individual Command - its title, DnD button, delete/edit icons.
 // This is looped over in SideBar to display each Command.
-// Clicking the 'Activate Command' button will add the command to activeCommands and redirect to /commands/generate.
+// Clicking the 'Activate Command' "+" button will add the command to activeCommands and redirect to /commands/generate.
+// Clicking the "edit" icon will redirect the user to /commands/edit and set that command to edit redux state.
 
 import React, { useState, useRef, useEffect } from "react";
 import { useDispatch } from "react-redux";
