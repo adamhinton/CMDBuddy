b576ab9b70506d06f614d4ca9ef03580d9be0c16
adamhinton <adam.d.hinton@gmail.com>
2024-01-02 18:38:42 -0500

    helpful comments

--- src/app/commands/edit/page.tsx
+++ src/app/commands/edit/page.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+// README:
+// User goes to this page to edit an existing Command
+// This saves parameter/command specs to the db.
+// This is different from the client-side process of generating commands by supplying values for parameters - that's done at commands/generate with CommandExecutionForm.tsx.
+
 import { Amplify } from "aws-amplify";
 import config from "../../../aws-exports";
 import { useSelector } from "react-redux";
