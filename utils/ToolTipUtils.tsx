cdcd21da61a21f73a705dbe7562ff542956d3e7e
adamhinton <adam.d.hinton@gmail.com>
2024-01-06 20:18:08 -0500

    reusable tooltip container util

---
+++ utils/ToolTipUtils.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+
+interface CMDBuddyTooltipProps {
+	content: string;
+	children: React.ReactElement;
+}
+
+const CMDBuddyTooltip: React.FC<CMDBuddyTooltipProps> = ({
+	content,
+	children,
+}) => {
+	return (
+		<Tippy
+			content={content}
+			popperOptions={{
+				modifiers: [
+					{
+						name: "offset",
+						options: {
+							offset: [0, 10], // Adjust x and y offsets as needed
+						},
+					},
+				],
+			}}
+		>
+			{children}
+		</Tippy>
+	);
+};
+
+export default CMDBuddyTooltip;
