1334f47574a8b373fc47216731da2b639596abb3
adamhinton <adam.d.hinton@gmail.com>
2024-01-08 10:29:00 -0500

    dummy ToC

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Amplify } from "aws-amplify";
 import config from "../../aws-exports";
 Amplify.configure({ ...config, ssr: true });
@@ -9,3 +7,24 @@ const About = () => {
 };
 
 export default About;
+
+type TableOfContentProps = [{ title: string }];
+const TableOfContents = (headings: TableOfContentProps) => {
+	return (
+		<nav>
+			<ul>
+				{headings.map((heading) => (
+					<li key={heading.title}>
+						<a href={`#${heading.title}`}>{heading.title}</a>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
+};
+
+const headings: TableOfContentProps = [
+	{
+		title: "Mission Statement",
+	},
+];
