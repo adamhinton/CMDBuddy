10c7800f77e46e484397ddabe8d47c7208a38d8f
adamhinton <adam.d.hinton@gmail.com>
2024-01-10 11:18:02 -0500

    README notes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
+// README:
+// Pretty straightforward About page.
+// ToC on the left has headings that, on click, jump user to relevant section
+// Has Subtitles and SubSubtitles
+// When you change headings, make sure to update in the ToC and add an href to their title
+
 /* eslint-disable react/no-unescaped-entities */
 "use client";
 
