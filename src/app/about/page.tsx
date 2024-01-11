77fb71b1116bc7fa8228d42a37b6f59c82723fde
adamhinton <adam.d.hinton@gmail.com>
2024-01-11 17:51:47 -0500

    delete more unused code

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,7 +16,6 @@ import { Amplify } from "aws-amplify";
 import config from "../../aws-exports";
 import Link from "next/link";
 Amplify.configure({ ...config, ssr: true });
-import styled, { useTheme } from "styled-components";
 import Image from "next/image";
 // These two image show command generation UI
 import darkModeGenMakeNewPersonIMG from "../../../utils/images/generatemakenewperson-darkmode.png";
