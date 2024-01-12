8898c39374c9c579eaeee2e35e54ffe4e5c8f06d
adamhinton <adam.d.hinton@gmail.com>
2024-01-12 18:11:24 -0500

    TODO item

--- utils/styles/AboutPageStyles.tsx
+++ utils/styles/AboutPageStyles.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import Image from "next/image";
 
+// TODO: Better link color in /about/page
+
 const Title = styled.h1`
 	color: ${({ theme }) => theme.aboutPage.title};
 `;
