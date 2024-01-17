51fa910e7c03f1f4a263b52bf2e3f3782a1043c2
adamhinton <adam.d.hinton@gmail.com>
2024-01-17 14:06:46 -0500

    TODO item

--- utils/MiscellaneousGlobalVariables.ts
+++ utils/MiscellaneousGlobalVariables.ts
@@ -2,6 +2,8 @@
 // This is to store re-usable variables
 // A single source of truth for the app on things that might change.
 
+// TODO: Add any other global variables
+
 // The max number of Commands a user can have in the DB
 // This is to prevent spam but can be raised if users need it
 export const MAX_COMMAND_LIMIT_PER_USER = 50;
