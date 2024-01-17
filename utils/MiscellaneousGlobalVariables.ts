351b7dbe5b5702492ddbb73fc55100550e2fe073
adamhinton <adam.d.hinton@gmail.com>
2024-01-17 15:33:45 -0500

    Update TODO items

--- utils/MiscellaneousGlobalVariables.ts
+++ utils/MiscellaneousGlobalVariables.ts
@@ -2,7 +2,7 @@
 // This is to store re-usable variables
 // A single source of truth for the app on things that might change.
 
-// TODO: Add any other global variables
+// TODO Stretch: Add any other global variables
 
 // The max number of Commands a user can have in the DB
 // This is to prevent spam but can be raised if users need it
