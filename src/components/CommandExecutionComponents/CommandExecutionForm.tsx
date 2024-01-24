59806b3ffa9f40b17b823513a92c9e9ea32c3244
adamhinton <adam.d.hinton@gmail.com>
2024-01-24 12:00:29 -0500

    del old code

--- src/components/CommandExecutionComponents/CommandExecutionForm.tsx
+++ src/components/CommandExecutionComponents/CommandExecutionForm.tsx
@@ -19,7 +19,7 @@ import { removeSingleActiveCommand } from "../../../redux/slices/activeCommandsS
 import { useDispatch } from "react-redux";
 import ParameterExecutionForm from "./ParameterExecutionForm";
 import React from "react";
-import { useForm, FormProvider, useFormContext } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import LiveCommandExecutionPreview from "./LiveCommandExecutionPreview";
 import CEFStyles from "../../../utils/CommandExecutionUtils/CommandExecutionStyles";
 
