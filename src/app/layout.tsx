8fa91eb0b8d5dc9174730f964f401904b2224416
adamhinton <adam.d.hinton@gmail.com>
2024-02-01 10:57:33 -0800

    CMDbuddy login is broken; temp maintenance message while I fix this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,8 @@ import Tabs from "@/components/Tabs";
 import ThemeProviderWrapper from "@/components/ThemeProviderWrapper";
 // Using react-toastify to alert user of various things with toasts.
 import ToastWrapper from "../../utils/ToastWrapper";
+import Link from "next/link";
+import styled from "styled-components";
 
 export const metadata = {
 	title: "CMDBuddy",
@@ -37,10 +39,7 @@ export default async function RootLayout({
 					<Providers>
 						{/* ThemeProviderWrapper also contains GlobalStyles */}
 						<ThemeProviderWrapper>
-							<AuthClientComponent />
-							<Header />
-							<Tabs />
-							<main>{children}</main>
+							<span></span>
 						</ThemeProviderWrapper>
 					</Providers>
 				</ToastWrapper>
